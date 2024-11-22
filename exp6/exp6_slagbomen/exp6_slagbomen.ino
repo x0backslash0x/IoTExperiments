7b2eaf7a9b2f33c3e893577f1a12a69da68200c2
Patrick Lanove <patrick.lanove@student.ap.be>
2024-11-22 11:44:07 +0100

    logica voor servomotor naar functie verplaatsen

--- exp6/exp6_slagbomen/exp6_slagbomen.ino
+++ exp6/exp6_slagbomen/exp6_slagbomen.ino
@@ -4,6 +4,24 @@
 Servo servoMotor;
 const int servoPin = 10; // pin D10 on an Arduino Nano board
 
+void RotateServo(Servo servo, int maxRotation, bool direction) {
+  if (direction) {
+    for (int pos = 0; pos <= maxRotation; pos +=1) { // in steps of 1 degree
+      servo.write(pos);
+      Serial.print("positive rotation ");
+      Serial.print(pos + "  ");
+      delay(15); // waits 15ms to reach the position
+    }
+  } else {
+    for (int pos = maxRotation; pos >= 0; pos -=1) { // in steps of 1 degree
+      servo.write(pos);
+      Serial.print("negative rotation ");
+      Serial.print(pos + "  ");
+      delay(15); // waits 15ms to reach the position
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   servoMotor.attach(servoPin); // attaches the servo to pin D10
@@ -11,19 +29,6 @@ void setup() {
 }
 
 void loop() {
-  // roates from 0 degrees to 180 degrees
-  for (int pos = 0; pos <= 180; pos +=1) { // in steps of 1 degree
-    servoMotor.write(pos);
-    Serial.print("positive rotation ");
-    Serial.print(pos + "  ");
-    delay(15); // waits 15ms to reach the position
-  }
-
-  // rotates from 180 degrees to 0 degrees
-  for (int pos = 180; pos >= 0; pos -=1) { // in steps of 1 degree
-    servoMotor.write(pos);
-    Serial.print("negative rotation ");
-    Serial.print(pos + "  ");
-    delay(15); // waits 15ms to reach the position
-  }
+  RotateServo(servoMotor, 180, true); // roates from 0 degrees to 180 degrees
+  RotateServo(servoMotor, 180, false); // roates from 180 degrees to 0 degrees
 }
